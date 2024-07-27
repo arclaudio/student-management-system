f787044b9802191b3b2a5ddf51783edc356b0a3e
clauds <arclaudio11@gmail.com>
2024-07-27 22:26:47 +0800

    Create Student Repository

--- src/main/java/com/arclaudio/sms/repository/StudentRepository.java
+++ src/main/java/com/arclaudio/sms/repository/StudentRepository.java
@@ -1,2 +1,7 @@
-package com.arclaudio.sms.repository;public class StudentRepository {
+package com.arclaudio.sms.repository;
+
+import com.arclaudio.sms.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
